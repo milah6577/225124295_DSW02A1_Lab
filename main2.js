5799f8dcb78bd65878d2f829a4398df9f962e98a
lindo <lindomathonsi25@gmail.com>
2026-03-11 22:57:56 +0200

    first commit

---
+++ main2.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let addBtn = document.getElementById("add");
+    let removeBtn = document.getElementById("remove");
+    let input = document.getElementById("item");
+    let list = document.getElementById("list");)
