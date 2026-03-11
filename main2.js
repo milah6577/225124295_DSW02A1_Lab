8eb149c596d8935608ef933fec6754a2a09dbfcb
lindo <lindomathonsi25@gmail.com>
2026-03-11 23:45:40 +0200

    first commit

--- main2.js
+++ main2.js
@@ -2,4 +2,32 @@ document.addEventListener("DOMContentLoaded", function () {
     let addBtn = document.getElementById("add");
     let removeBtn = document.getElementById("remove");
     let input = document.getElementById("item");
-    let list = document.getElementById("list");)
+    let list = document.getElementById("list");
+    let removedItemsDiv = document.getElementById('removed-items');
+    let removeCountDiv = document.getElementById('remove-count');
+
+    let removeCount = 0;
+    let removedItems = [];
+
+    function alternatingColours(){
+        let list = document.getElementById("list");
+        let items = document.getElementById("items");
+
+        for(let i = 0; i< items.length; i++){
+            if(i%2 === 0){
+                items[i].className = "white";
+            }
+            else{
+                items[i].className = "yellow"
+            }
+
+        }
+    }
+    addBtn.addEventListener("click",function()){
+let text = itemsinput.value;
+     }
+
+   removeBtn.addEventListener("click", function()){
+let 
+   }
+})
